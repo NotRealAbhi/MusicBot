6b7daa9b254a6482639e0e99543bab8c079bc7d5
Abhishek Thakur <alaricabhi@gmail.com>
2025-04-10 00:19:27 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,13 @@
+import asyncio
+from Call.Player.Calls import bot, call
+
+async def main():
+    await bot.start()
+    await call.start()
+    print("✅ Music Bot and VC Engine started!")
+
+    await idle()  # Keeps the bot running
+
+if __name__ == "__main__":
+    from pyrogram.idle import idle
+    asyncio.run(main())
