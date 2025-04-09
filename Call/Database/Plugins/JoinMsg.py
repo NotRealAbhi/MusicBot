2aa7fbda9aafc74da38a83189adf40eda42fd407
Abhishek Thakur <alaricabhi@gmail.com>
2025-04-09 23:33:04 +0530

    Update JoinMsg.py

--- Call/Database/Plugins/JoinMsg.py
+++ Call/Database/Plugins/JoinMsg.py
@@ -1,1 +1,23 @@
+"""
+Telegram @NotRealBhi
+Copyright ©️ 2025
+"""
 
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from Call.Player.Core import send_welcome_message  # Assuming we have this method in Core
+
+@Client.on_message(filters.new_chat_members)
+async def welcome_new_user(client: Client, message: Message):
+    """Sends a custom welcome message when a user joins the voice chat."""
+    if message.chat.type != "private":
+        new_member = message.new_chat_members[0]
+        chat_id = message.chat.id
+
+        # Get the custom join message (can be saved in DB or config)
+        join_message = "Welcome to the Voice Chat, {first_name}! 🎧"
+
+        # Send the custom join message
+        formatted_message = join_message.format(first_name=new_member.first_name)
+        await send_welcome_message(chat_id, formatted_message)
+      
