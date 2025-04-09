46ae16d4efd33ec8b4c0f2db6bbf5600812ed024
Abhishek Thakur <alaricabhi@gmail.com>
2025-04-10 01:10:00 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,19 +8,5 @@ async def start(client, message):
     await message.reply("Bot is online!")
 
 
-async def start_bot():
-    try:
-        await bot.start()  # Start the bot
-        print("✅ Bot started successfully!")
-    except Exception as e:
-        print(f"⚠️ Error starting the bot: {e}")
-
-    # Running the PyTgCalls bot in a loop
-    await call.start()
-
-# Run the bot
-if __name__ == "__main__":
-    import asyncio
-    asyncio.run(start_bot())
-
-
+bot.run()
+call.start()
