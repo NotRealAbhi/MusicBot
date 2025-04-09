9feeba477cb55ce9af828a3b06f7a78a8e6b2f18
Abhishek Thakur <alaricabhi@gmail.com>
2025-04-09 23:18:02 +0530

    Update P&R.py

--- Call/Database/Plugins/P&R.py
+++ Call/Database/Plugins/P&R.py
@@ -1,1 +1,25 @@
+"""
+Telegram @NotRealBhi
+Copyright ©️ 2025
+"""
 
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from Call.Player.Core import pause, resume
+
+
+@Client.on_message(filters.command("pause") & filters.group)
+async def pause_command(client: Client, message: Message):
+    """Pauses the current song."""
+    chat_id = message.chat.id
+    result = await pause(chat_id)
+    await message.reply(result)
+
+
+
+@Client.on_message(filters.command("resume") & filters.group)
+async def resume_command(client: Client, message: Message):
+    """Resumes the paused song."""
+    chat_id = message.chat.id
+    result = await resume(chat_id)
+    await message.reply(result)
