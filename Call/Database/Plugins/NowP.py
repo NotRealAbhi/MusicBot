3e07a4d5aee4f27a352b392aa6621e047f5f4c3c
Abhishek Thakur <alaricabhi@gmail.com>
2025-04-09 23:08:29 +0530

    Update NowP.py

--- Call/Database/Plugins/NowP.py
+++ Call/Database/Plugins/NowP.py
@@ -1,1 +1,16 @@
+"""
+Telegram @NotRealBhi
+Copyright ©️ 2025
+"""
 
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from Call.Player.Core import get_now_playing
+
+
+@Client.on_message(filters.command("now") & filters.group)
+async def nowplaying(client: Client, message: Message):
+    """Displays the current song and seek bar."""
+    response = await get_now_playing()
+    await message.reply(response)
+  
